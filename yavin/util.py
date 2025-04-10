ef387b4a498e35ceb1159f75dfc1e942d1ee0c65
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-04-10 11:35:24 -0500

    Remove unused function `clean_due_date()`

--- yavin/util.py
+++ yavin/util.py
@@ -10,10 +10,6 @@ def clean_datetime(d: datetime.datetime) -> str:
     return d.strftime("%Y-%m-%d %H:%M")
 
 
-def clean_due_date(due: str) -> datetime.date:
-    return datetime.datetime.strptime(due, "%m\u2011%d\u2011%Y").date()
-
-
 def in_two_minutes() -> datetime.datetime:
     return datetime.datetime.now() + datetime.timedelta(minutes=2)
 
