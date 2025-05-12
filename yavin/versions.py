ce884e8a73e1a1be69504c063fc3572c094fe26f
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-05-12 10:30:26 -0500

    Simplify reading package.json

--- yavin/versions.py
+++ yavin/versions.py
@@ -1,9 +1,7 @@
 import json
 import pathlib
 
-package_json = pathlib.Path().resolve() / "package.json"
-with package_json.open() as f:
-    data = json.load(f)
+data = json.loads(pathlib.Path("package.json").read_text())
 
 bi: str = data.get("dependencies").get("bootstrap-icons")
 bs: str = data.get("dependencies").get("bootstrap")
